82b441328aaeccb50154710ba790dcb06fdaa2f2
princeparvat80 <princeparvat80@gmail.com>
2021-06-29 13:38:02 +0530

    for loop

--- Basics/forloop.js
+++ Basics/forloop.js
@@ -1,6 +1,25 @@
+//---------------------------------------------------------Normal for Loop-----------------------------
 var num= 13;
 var res;
 for(x=1;x<=10;x++){
  res= num * x;
  console.log(num + "*" + x + "=" + res);
+}
+
+//---------------------------------------- for in (use to pick data from object)-------------------------------------
+
+var object = {'name':'Prince' ,'address':'BUI','phn num':'54784' };
+for(data in object){
+    console.log(data);
+    //this will give the keys
+}
+
+for(let data in object){
+    console.log(object[data]);
+    //this will give the values
+}
+
+//------------------------------------------for of --------------------------------------------------------
+for (let item of ['prince','35','2651.023']){
+    console.log(item);
 }
